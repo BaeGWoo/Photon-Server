49d7654931d39fad0366c970415997422ab9ccda
BaeGWoo <gwbae4211@naver.com>
2022-09-20 20:08:21 +0900

    Photon Setting

---
+++ Assets/Script/PhotonSetting.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using PlayFab;
+using PlayFab.ClientModels;
+//using TMPro;
+
+
+public class PhotonSetting : MonoBehaviour
+{
+    [SerializeField] InputField email;
+    [SerializeField] InputField password;
+    [SerializeField] InputField username;
+    [SerializeField] Dropdown region;
+    //public TMP_Dropdown
+
+    void Start()
+    {
+      
+    }
+
+
+    //LoginResult <- 로그인 성공 여부 반환(playfab에 내장되어있다.)
+    public void LoginSuccess(LoginResult result)
+    {
+        //마스터 클라이언트를 기준으로 씬을 동기화할 지 안할지 결정하는 기능
+        //fasle=동기화를 하지 않겠다.
+        PhotonNetwork.AutomaticallySyncScene = false;
+
+        //같은 버전의 유저끼리 접속을 허용합니다.
+        PhotonNetwork.GameVersion = "1.0f";
+
+        //유저 아이디 설정
+        PhotonNetwork.NickName = username.text;
+
+        //입력한 지역을 설정합니다.
+        //dropdown형태일 경우, 
+        PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion = region.options[region.value].text;
+
+    }
+
+    public void LoginFailure(PlayFabError error)
+    {
+        Debug.Log("로그인 실패");
+    }
+    
+    public void SignUpSuccess(RegisterPlayFabUserResult result)
+    {
+        Debug.Log("회원가입 성공");
+    }
+
+    public void SignUpFailure(PlayFabError error)
+    {
+        Debug.Log("회원가입 실패");
+    }
+
+    public void SignUp()
+    {
+        //RegisterPlayFabUserRequest : 서버에 유저를 등록하기 위한 클래스
+        var request = new RegisterPlayFabUserRequest
+        {
+            Email = email.text,
+            Password = password.text,
+            Username = username.text
+        };
+
+        PlayFabClientAPI.RegisterPlayFabUser
+        (
+            request,
+            SignUpSuccess,
+            SignUpFailure
+        );
+    }
+   
+
+}
