9046dbec6f9d7f2e6ba0885c1716ebe21eb1efa4
BaeGWoo <gwbae4211@naver.com>
2022-10-10 19:47:25 +0900

    Playfab 가상화폐 통화

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -4,6 +4,12 @@ using PlayFab.ClientModels;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+// L value 와 R value
+// l value : 표현식 이후에도 사라지지 않은 값 ( 메모리 공간을 가지고 있는 변수)
+//R  value : 표현식 이후에는 사라지는 값 ( 임시 변수)
+
+
 public class UIManager : MonoBehaviour
 {
     public static UIManager instance;
@@ -17,6 +23,56 @@ public class UIManager : MonoBehaviour
     {
         if (instance == null)
             instance = this;
+    }
+
+    [SerializeField] GameObject leaderBorder;
+
+    
+
+    public void GetLeaderBorder()
+    {
+        leaderBorder.SetActive(true);
+
+        var request = new GetLeaderboardRequest
+        {
+            StartPosition = 0, // 기본 위치 값 
+            StatisticName = "Score", // Playfab에서 불러올 순위표 이름
+            MaxResultsCount = 20, // 순위표에 최대로 나타나는 수
+            ProfileConstraints = new PlayerProfileViewConstraints()
+            {
+                ShowLocations = true,
+                ShowDisplayName = true
+            }
+        };
+
+        PlayFabClientAPI.GetLeaderboard(request, (result) =>
+        {
+            for (int i = 0; i < result.Leaderboard.Count; i++)
+            {
+                var currentBoader = result.Leaderboard[i];
+
+                leaderBorderText.text += currentBoader.Profile.Locations[0].CountryCode.Value +
+                " - " + currentBoader.DisplayName + " - " + currentBoader.StatValue + "\n";
+            }
+        },
+        (error) => Debug.Log("리더보드를 불러오지 못했습니다."));
+    }
+
+
+    public void GetVirtualCurrency()
+    {
+        var request = new AddUserVirtualCurrencyRequest()
+        {
+            VirtualCurrency = "RP",//Playfab에 설정한 통화코드
+            Amount = 100 //가상 화폐에 추가할 값
+        };
+
+        PlayFabClientAPI.AddUserVirtualCurrency
+            (
+            //result.Balance 현재 가상화폐 돈
+                request, (result) => Debug.Log("가상 화폐가 추가되었습니다."+ result.Balance),
+                (error)=>Debug.Log("가상 화폐를 획득하지 못했습니다.")
+            );
     }
 
 }
--- Assets/Script/WebLoad.cs
+++ Assets/Script/WebLoad.cs
@@ -5,18 +5,29 @@ using UnityEngine.UI;
 using UnityEngine.Networking;
 
 
+//eddabf98-70df-48a2-aade-8d4b2365f15d
+
 public class WebLoad : MonoBehaviour
 {
     //웹에서 이미지를 불러올 때는 Raw Image를 사용해야 합니다.
     [SerializeField] RawImage[] webImage;
+
+    [SerializeField] string[] imageAddress;
+
 
 
     //Awake : 게임 시작 전 데이터를 로드할 때 사용
     //Start : 캐릭터 위치 값 등 설정 후 게임 실행
     void Awake()
     {
-        string a= "https://github.com/Unity2033/Unity-3D-Example/blob/main/Assets/Class/Photon%20Server/Texture/Ice%20Kingdom.jpg?raw=true";
-        StartCoroutine(WebImageLoad(a));
+        imageAddress[0]= "https://github.com/Unity2033/Unity-3D-Example/blob/main/Assets/Class/Photon%20Server/Texture/Ice%20Kingdom.jpg?raw=true";
+        imageAddress[1] = "https://raw.githubusercontent.com/Unity2033/Unity-3D-Example/main/Assets/Class/Photon%20Server/Texture/Leader%20Board%20Panel.png";
+        imageAddress[2] = "https://raw.githubusercontent.com/Unity2033/Unity-3D-Example/main/Assets/Class/Photon%20Server/Texture/Leader%20Board%20Panel.png";
+        imageAddress[3] = "https://raw.githubusercontent.com/Unity2033/Unity-3D-Example/main/Assets/Class/Photon%20Server/Texture/Leader%20Board%20Panel.png";
+        for (int i = 0; i < imageAddress.Length; i++)
+        {
+            StartCoroutine(WebImageLoad(imageAddress[i]));
+        }
     }
 
     private void Start()
@@ -40,8 +51,10 @@ public class WebLoad : MonoBehaviour
 
         else
         {
-            for(int i=0;i<webImage.Length;i++)
-            webImage[i].texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            for (int i = 0; i < webImage.Length; i++)
+            {
+                webImage[i].texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            }
         }
 
     }
